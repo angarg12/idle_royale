5fdd7541d13fc3cc61363c382c02c9a517cec743
Andrés <angarg12@gmail.com>
2017-02-12 21:17:16 +0100

    All upgrades should work

--- src/services/generator.js
+++ src/services/generator.js
@@ -25,27 +25,58 @@ function Generator(actor, opponent) {
 		},
 		'Tier 2':{
 			price:100,
-			power:10
+			power:10,
+			upgrades: [
+			  "Tier 2-1",
+			  "Tier 2-2",
+			  "Tier 2-3",
+			  "Tier 2-4"
+			]
 		},
 		'Tier 3':{
 			price:1100,
-			power:80
+			power:80,
+			upgrades: [
+			  "Tier 3-1",
+			  "Tier 3-2",
+			  "Tier 3-3",
+			  "Tier 3-4"
+			]
 		},
 		'Tier 4':{
 			price:12000,
-			power:470
+			power:470,
+			upgrades: [
+			  "Tier 4-1",
+			  "Tier 4-2",
+			  "Tier 4-3",
+			  "Tier 4-4"
+			]
 		},
 		'Tier 5':{
 			price:130000,
-			power:2600
+			power:2600,
+			upgrades: [
+			  "Tier 5-1",
+			  "Tier 5-2",
+			  "Tier 5-3"
+			]
 		},
 		'Tier 6':{
 			price:1400000,
-			power:14000
+			power:14000,
+			upgrades: [
+			  "Tier 6-1",
+			  "Tier 6-2",
+			  "Tier 6-3"
+			]
 		},
 		'Tier 7':{
 			price:2000000,
-			power:78000
+			power:78000,
+			upgrades: [
+			  "Tier 7-1"
+			]
 		},
 		'Tier 8':{
 			price:330000000,
