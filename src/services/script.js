745cf8baafbc198c7060969d0750857d7e75c90a
Andrés <angarg12@gmail.com>
2017-02-12 18:48:06 +0100

    Improved error messages

--- src/services/script.js
+++ src/services/script.js
@@ -23,7 +23,26 @@ function Script(generator, upgrade, spell) {
 	  try{
 	    eval(this.script);
 	  }catch(error){
-	    return error;
+		  var message = error.name+" "+error.message;
+		  if(error.fileName){
+			  message += "<br> File: "+error.fileName;
+		  }
+		  if(error.lineNumber){
+			  message += "<br> Line number: "+error.lineNumber;
+		  }
+		  if(error.columnNumber){
+			  message += "<br> Column: "+error.columnNumber;
+		  }
+		  if(error.stack){
+			  message += "<br> Stack: "+error.stack;
+		  }
+		  if(error.description){
+			  message += "<br> Description: "+error.description;			  
+		  }
+		  if(error.number){
+			  message += "<br> Number: "+error.number;
+		  }
+	    return message;
 	  }
   };
 };
