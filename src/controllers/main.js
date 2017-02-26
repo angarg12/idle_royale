80e134441c40cc8696ec9cd9bf953f103d997774
Andrés <angarg12@gmail.com>
2017-02-26 12:17:40 +0100

    Fixed #5

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,7 +1,6 @@
 angular
 .module('incremental')
-.controller('IncCtrl',
-['$scope',
+.controller('IncCtrl', ['$scope',
 '$document',
 '$interval',
 '$sce',
@@ -20,246 +19,236 @@ angular
 'upgradeEnemy',
 'spellEnemy',
 'scriptEnemy',
-function ($scope, $document, $interval, $sce, $filter, $timeout, data, util, savegame, player, generator, upgrade, spell, script, enemy, generatorEnemy, upgradeEnemy, spellEnemy, scriptEnemy) {
-  $scope.version = '0.8.4';
-  $scope.Math = window.Math;
-  
-  $scope.data = data;
-  $scope.util = util;
-  $scope.savegame = savegame;
-  $scope.player = player;
-  $scope.generator = generator;
-  $scope.upgrade = upgrade;
-  $scope.spell = spell;
-  $scope.script = script;
-  $scope.enemy = enemy;
-  $scope.generatorEnemy = generatorEnemy;
-  $scope.upgradeEnemy = upgradeEnemy;
-  $scope.spellEnemy = spellEnemy;
-  var self = this;
+function($scope, $document, $interval, $sce, $filter, $timeout, data, util, savegame, player, generator, upgrade, spell, script, enemy, generatorEnemy, upgradeEnemy, spellEnemy, scriptEnemy) {
+    $scope.version = '0.8.5';
+    $scope.Math = window.Math;
 
-  var player_script = "generator.buyGenerators('Tier 1',1);";
-  var enemy_script = "if(actor.power < 0.004*goal){\
-var gens = generator.getKeys();\
-for(var i = gens.length; i > 0; i--){\
-  var number = generator.maxBuy(gens[i-1]);\
-  generator.buyGenerators(gens[i-1],number);\
-}\
-var ups = upgrade.getKeys();\
-for(var i = ups .length; i > 0; i--){\
-  upgrade.buyUpgrade(ups[i-1]);\
-}\
-}\
-if(production > 167){\
-  spell.activateSpell('Surge');\
-}\
-if(production > 334){\
-  spell.activateSpell('Drain');\
-}";
-  
-  player.setScope($scope);
-  enemy.setScope($scope);
-  util.setScope($scope);
-  savegame.setScope($scope);
+    $scope.data = data;
+    $scope.util = util;
+    $scope.savegame = savegame;
+    $scope.player = player;
+    $scope.generator = generator;
+    $scope.upgrade = upgrade;
+    $scope.spell = spell;
+    $scope.script = script;
+    $scope.enemy = enemy;
+    $scope.generatorEnemy = generatorEnemy;
+    $scope.upgradeEnemy = upgradeEnemy;
+    $scope.spellEnemy = spellEnemy;
+    var self = this;
 
-  $scope.tierProduction = function (actor, opponent, generator, upgrade, name) {
-	if(!actor.data) return;
-    var baseProduction = generator.getGenerators()[name].power *
-                         actor.data.generators[name].level;
-    return $scope.upgradedProduction(actor, opponent, generator, upgrade, baseProduction, name);
-  };
-  
-  $scope.totalProduction = function (actor, opponent, generator, upgrade) {
-	if(!actor.data) return;
-    var total = 0;
-    for(var tier in generator.getGenerators()) {
-      total += this.tierProduction(actor, opponent, generator, upgrade, tier);
-    }
-	if(actor.data.spells["Surge"].active){
-	  total *= 1.1;
-	}
-	if(opponent.data.spells["Drain"].active){
-	  total *= 0.9;
-	}
-    return total;
-  };
-  
-  $scope.upgradedProduction = function (actor, opponent, generator, upgrade, production, name) {
-	if(actor.data.spells["Humility"].active 
-    || opponent.data.spells["Humility"].active ){
-		return production;
-	}
-	var generators = generator.getGenerators();
-	var upgrades = upgrade.getUpgrades();
-    for(var upgrade in generators[name].upgrades) {
-        if(actor.data.upgrades[generators[name].upgrades[upgrade]].bought) {
-          power = upgrades[generators[name].upgrades[upgrade]].power;
-          production = production * power;
+    player.setScope($scope);
+    enemy.setScope($scope);
+    util.setScope($scope);
+    savegame.setScope($scope);
+
+    $scope.tierProduction = function(actor, opponent, generator, upgrade, name) {
+        if (!actor.data) return;
+        var baseProduction = generator.getGenerators()[name].power *
+            actor.data.generators[name].level;
+        return $scope.upgradedProduction(actor, opponent, generator, upgrade, baseProduction, name);
+    };
+
+    $scope.totalProduction = function(actor, opponent, generator, upgrade) {
+        if (!actor.data) return;
+        var total = 0;
+        for (var tier in generator.getGenerators()) {
+            total += this.tierProduction(actor, opponent, generator, upgrade, tier);
         }
-      }
-      return production;
-  };
-  
-  self.processSpells = function(actor) {
-    for(var spell in actor.data.spells){
-	  if(actor.data.spells[spell].active){
-	    actor.data.spells[spell].duration--;
-	  }else if(actor.data.spells[spell].cooldown > 0){
-		actor.data.spells[spell].cooldown--;
-	  }
-	  if(actor.data.spells[spell].duration <= 0){
-	    actor.data.spells[spell].active = false;
-	  }
-	}
-  };
-  
-  self.processProduction = function (actor, opponent, generator, upgrade) {
-	actor.data.power += $scope.totalProduction(actor, opponent, generator, upgrade);
-  };  
-  
-  self.update = function () {
-    data.save.scripts[$scope.current_slot] = self.code.getValue();
-	if($scope.status === 'play' || $scope.status === 'fast'){		
-		self.processProduction(player, enemy, generator, upgrade);
-		self.processProduction(enemy, player, generatorEnemy, upgradeEnemy);
-		
-		if(player.data.power >= $scope.goal 
-		  && enemy.data.power >= $scope.goal){
-		  $scope.result = "tie";
-		  return;
-		}else if(player.data.power >= $scope.goal){
-		  $scope.result = "win";
-		  data.save.rounds[$scope.current_enemy].wins++;
-		  if(!data.save.rounds[$scope.current_enemy].record 
-		    || $scope.turn < data.save.rounds[$scope.current_enemy].record){
-		    data.save.rounds[$scope.current_enemy].record = $scope.turn;
-		  }
-		  return;
-		}else if(enemy.data.power >= $scope.goal){
-		  $scope.result = "lose";
-		  return;
-		}
-		
-		self.processSpells(player);
-		self.processSpells(enemy);
-		var opponent = angular.copy(enemy.data);
-		opponent.script = undefined;
-		$scope.error_msg = script.eval(angular.copy(player.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(player, enemy, generator, upgrade));
-		generator.clear();
-		upgrade.clear();
-		spell.clear();
-		var opponent = angular.copy(player.data);
-		opponent.script = undefined;
-		scriptEnemy.eval(angular.copy(enemy.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(enemy, player, generator, upgrade));
-		generator.clear();
-		upgrade.clear();
-		spell.clear();
-		$scope.turn++;
-	}
-  
-    $interval(self.update, $scope.game_speed, 1);
-  };
-  
-  $scope.run = function() {
-    if($scope.status === "stop"){
-      if(!script.script){
-        alert("Script is empty");
-      }
+        if (actor.data.spells["Surge"].active) {
+            total *= 1.1;
+        }
+        if (opponent.data.spells["Drain"].active) {
+            total *= 0.9;
+        }
+        return total;
+    };
 
-      if($scope.error_msg){
-        return;
-      }
-      $scope.status = "play";
-    }  
-  };
-  
-  $scope.toggleFast = function() {
-    if($scope.status === "play"){
-      $scope.status = "fast";
-      $scope.game_speed = 1;
-    }else if($scope.status === "fast"){
-      $scope.status = "play";
-      $scope.game_speed = 1000;
-    }    
-  };
-  
-  $scope.loadScript = function() {
-    script.clearCache();
-    script.script = self.code.getValue();
+    $scope.upgradedProduction = function(actor, opponent, generator, upgrade, production, name) {
+        if (actor.data.spells["Humility"].active ||
+            opponent.data.spells["Humility"].active) {
+            return production;
+        }
+        var generators = generator.getGenerators();
+        var upgrades = upgrade.getUpgrades();
+        for (var up in generators[name].upgrades) {
+            if (actor.data.upgrades[generators[name].upgrades[up]].bought) {
+                power = upgrades[generators[name].upgrades[up]].power;
+                production = production * power;
+            }
+        }
+        return production;
+    };
 
-    self.codeoutput.setValue(script.script);
-    setTimeout(function() {
-      self.codeoutput.refresh();
-    },1);
-    var opponent = angular.copy(enemy.data);
-		opponent.script = undefined;
-    $scope.error_msg = script.eval(angular.copy(player.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(player, enemy, generator, upgrade));
-  };
+    self.processSpells = function(actor) {
+        for (var spell in actor.data.spells) {
+            if (actor.data.spells[spell].active) {
+                actor.data.spells[spell].duration--;
+            } else if (actor.data.spells[spell].cooldown > 0) {
+                actor.data.spells[spell].cooldown--;
+            }
+            if (actor.data.spells[spell].duration <= 0) {
+                actor.data.spells[spell].active = false;
+            }
+        }
+    };
 
-  $scope.changeSlot = function() {
-    self.code.setValue(data.save.scripts[$scope.current_slot]);
-    setTimeout(function() {
-      self.code.refresh();
-    },1);
-  };
-  
-  $scope.init = function () {
-    player.populatePlayer();
-    enemy.populatePlayer();
-    $scope.current_tab = "Game";
-    $scope.current_slot = 1;
-    $scope.changeSlot();
-    $scope.turn = 0;
-    $scope.goal = 2e10;
-    $scope.game_speed = 1000;
-    $scope.error_msg = "";
-    // win, lose, tie
-    $scope.result = "";
-    // stop, play, fast, finish
-    $scope.status = "stop";
-    $scope.current_enemy = "Bot";
-    script.clearCache();    
-    enemy.script = enemy_script;
-    scriptEnemy.script = enemy_script;
-    scriptEnemy.clearCache();
-	};
+    self.processProduction = function(actor, opponent, generator, upgrade) {
+        actor.data.power += $scope.totalProduction(actor, opponent, generator, upgrade);
+    };
 
-  $scope.restart = function() {
-	var answer = confirm("Do you want to restart the round?");
-	if(answer){
-		$scope.init();
-    $interval(self.update, $scope.game_speed, 1);
-	}
-  };
-  
-  self.startup = function () {
-    savegame.init();
-    data.save.scripts[$scope.current_slot] = player_script;
-    
-    self.code = CodeMirror.fromTextArea(document.getElementById('code'), {
-      lineNumbers: true
-    });
-    self.codeoutput = CodeMirror.fromTextArea(document.getElementById('codeoutput'), {
-      lineNumbers: true,
-      readOnly: true,
-      theme: 'codemirror_readonly'
-    });
-    
-	  $scope.init();
-	  savegame.load();      
+    self.update = function() {
+        data.save.scripts[$scope.current_slot] = self.code.getValue();
 
-    self.code.setValue(data.save.scripts[$scope.current_slot]);
-    setTimeout(function() {
-      self.code.refresh();
-    },1);    
+        if ($scope.status === "restart") {
+            $scope.init();
+            this.main_loop = $interval(self.update, $scope.game_speed, 1);
+            return;
+        }
 
-    self.codeoutput.setValue("");
-    setTimeout(function() {
-      self.codeoutput.refresh();
-    },1);
-    $interval(self.update, $scope.game_speed, 1);
-    $interval(savegame.store, 10000);
-  };
-  
-  self.onload = $timeout(self.startup);  
-}]);
+        if ($scope.status === 'play' || $scope.status === 'fast') {
+            self.processProduction(player, enemy, generator, upgrade);
+            self.processProduction(enemy, player, generatorEnemy, upgradeEnemy);
+
+            if (player.data.power >= $scope.goal &&
+                enemy.data.power >= $scope.goal) {
+                $scope.result = "tie";
+                return;
+            } else if (player.data.power >= $scope.goal) {
+                $scope.result = "win";
+                data.save.rounds[$scope.current_enemy].wins++;
+                if (!data.save.rounds[$scope.current_enemy].record ||
+                    $scope.turn < data.save.rounds[$scope.current_enemy].record) {
+                    data.save.rounds[$scope.current_enemy].record = $scope.turn;
+                }
+                return;
+            } else if (enemy.data.power >= $scope.goal) {
+                $scope.result = "lose";
+                return;
+            }
+
+            self.processSpells(player);
+            self.processSpells(enemy);
+            var opponent = angular.copy(enemy.data);
+            opponent.script = undefined;
+            $scope.error_msg = script.eval(angular.copy(player.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(player, enemy, generator, upgrade));
+            generator.clear();
+            upgrade.clear();
+            spell.clear();
+            opponent = angular.copy(player.data);
+            opponent.script = undefined;
+            scriptEnemy.eval(angular.copy(enemy.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(enemy, player, generator, upgrade));
+            generator.clear();
+            upgrade.clear();
+            spell.clear();
+            $scope.turn++;
+        }
+
+        this.main_loop = $interval(self.update, $scope.game_speed, 1);
+    };
+
+    $scope.run = function() {
+        if ($scope.status === "stop") {
+            if ($scope.error_msg) {
+                return;
+            }
+            $scope.status = "play";
+        }
+    };
+
+    $scope.toggleFast = function() {
+        if ($scope.status === "play") {
+            $scope.status = "fast";
+            $scope.game_speed = 1;
+        } else if ($scope.status === "fast") {
+            $scope.status = "play";
+            $scope.game_speed = 1000;
+        }
+    };
+
+    $scope.loadScript = function() {
+        script.clearCache();
+        script.script = self.code.getValue();
+
+        self.codeoutput.setValue(script.script);
+        setTimeout(function() {
+            self.codeoutput.refresh();
+        }, 1);
+        var opponent = angular.copy(enemy.data);
+        opponent.script = undefined;
+        $scope.error_msg = script.eval(angular.copy(player.data), opponent, $scope.goal, $scope.turn, $scope.totalProduction(player, enemy, generator, upgrade));
+    };
+
+    $scope.changeSlot = function() {
+        self.code.setValue(data.save.scripts[$scope.current_slot]);
+        setTimeout(function() {
+            self.code.refresh();
+        }, 1);
+    };
+
+    $scope.changeEnemy = function() {
+      enemy.script = data.enemy_scripts[$scope.current_enemy];
+      scriptEnemy.script = enemy.script;
+      scriptEnemy.clearCache();
+    };
+
+    $scope.init = function() {
+        player.populatePlayer();
+        enemy.populatePlayer();
+        $scope.current_tab = "Game";
+        $scope.current_slot = 1;
+        $scope.changeSlot();
+        $scope.turn = 0;
+        $scope.goal = 2e10;
+        $scope.game_speed = 1000;
+        $scope.error_msg = "";
+        // win, lose, tie
+        $scope.result = "";
+        // stop, play, fast, finish, restart
+        $scope.status = "stop";
+        $scope.current_enemy = "Bot";
+        script.clearCache();
+        enemy.script = data.enemy_scripts[$scope.current_enemy];
+        scriptEnemy.script = enemy.script;
+        scriptEnemy.clearCache();
+    };
+
+    $scope.restart = function() {
+        var answer = confirm("Do you want to restart the round?");
+        if (answer) {
+          $scope.status = "restart";
+        }
+    };
+
+    self.startup = function() {
+        savegame.init();
+        data.save.scripts[$scope.current_slot] = data.player_script;
+
+        self.code = CodeMirror.fromTextArea(document.getElementById('code'), {
+            lineNumbers: true
+        });
+        self.codeoutput = CodeMirror.fromTextArea(document.getElementById('codeoutput'), {
+            lineNumbers: true,
+            readOnly: true,
+            theme: 'codemirror_readonly'
+        });
+
+        $scope.init();
+        savegame.load();
+
+        self.code.setValue(data.save.scripts[$scope.current_slot]);
+        setTimeout(function() {
+            self.code.refresh();
+        }, 1);
+
+        self.codeoutput.setValue("");
+        setTimeout(function() {
+            self.codeoutput.refresh();
+        }, 1);
+        this.main_loop = $interval(self.update, $scope.game_speed, 1);
+        $interval(savegame.store, 10000);
+    };
+
+    self.onload = $timeout(self.startup);
+}
+]);
